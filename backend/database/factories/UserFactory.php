39bf6a5cce126c302e90d792afd4f96d5ef2d07b
Nikko Lumahang <kertnik@gmail.com>
2020-10-03 22:05:37 +0800

    changed the bcrypt to Hash::make()

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
@@ -25,7 +26,7 @@ class UserFactory extends Factory
         return [
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
-            'password' => bcrypt('password'),
+            'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
