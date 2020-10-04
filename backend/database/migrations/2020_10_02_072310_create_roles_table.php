acaecc1464c2c186d9c34e27178f80bb75d16d7a
Nikko Lumahang <kertnik@gmail.com>
2020-10-04 21:01:20 +0800

    unique index added to name & deleted_at columns in roles table

--- backend/database/migrations/2020_10_02_072310_create_roles_table.php
+++ backend/database/migrations/2020_10_02_072310_create_roles_table.php
@@ -19,6 +19,7 @@ class CreateRolesTable extends Migration
             $table->string('description');
             $table->timestamps();
             $table->softDeletes();
+            $table->unique(['name', 'deleted_at']);
         });
     }
 
