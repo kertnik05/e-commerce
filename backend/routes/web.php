7caf687767173ca804de994b49b60f56e18bed30
Nikko Lumahang <kertnik@gmail.com>
2020-10-04 19:52:07 +0800

    moved apiResource routes to api.php

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -18,7 +18,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::resource('roles', RoleController::class)->except('show');
-Route::resource('users', UserController::class);
