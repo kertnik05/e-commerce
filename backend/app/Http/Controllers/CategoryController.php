22f124b65f37b96d4e038ded984bc7debec01202
Nikko Lumahang <kertnik@gmail.com>
2020-10-08 17:55:23 +0800

    API resource controller for category is added

---
+++ backend/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::latest()->paginate(10);
+        return CategoryResource::collection($categories);
+    }
+
+    public function store(StoreCategoryRequest $request)
+    {
+        Category::create($request->all());
+        return response()->json(['success' => true], 201);
+    }
+
+    public function show(Category $category)
+    {
+        return response()->json([
+            'data' => new CategoryResource($category),
+            'success' => true
+        ], 200);
+    }
+
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        $category->update($request->all());
+        return response()->json(['success' => true], 200);
+    }
+
+    public function destroy(Category $category)
+    {
+        if ($category->delete()) {
+            return response()->json(['success' => true], 200);
+        }
+    }
+}
