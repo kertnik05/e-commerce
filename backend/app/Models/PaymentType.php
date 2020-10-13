9c16bb75b927b66bc91e24f54631857b869d53af
Nikko Lumahang <kertnik@gmail.com>
2020-10-13 18:33:42 +0800

    hasMany relationship added in PaymentType model to checkout

--- backend/app/Models/PaymentType.php
+++ backend/app/Models/PaymentType.php
@@ -2,13 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Checkout;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class PaymentType extends Model
 {
     use HasFactory, SoftDeletes;
 
     protected $guarded = [];
+
+    public function checkouts()
+    {
+        return $this->hasMany(Checkout::class);
+    }
 }
