6a39cd623de56877e8f46633f3c7f1de8ce5f1ec
Nikko Lumahang <kertnik@gmail.com>
2020-10-22 21:42:45 +0800

    browser-sync added

--- frontend/webpack.mix.js
+++ frontend/webpack.mix.js
@@ -2,6 +2,10 @@ const mix = require('laravel-mix');
 
 require('vuetifyjs-mix-extension');
 
-mix.js('src/js/app.js', 'dist/js').vuetify('vuetify-loader')
-   .sass('src/sass/app.scss', 'dist/css')
-   .setPublicPath('dist');
+mix.js('src/js/app.js', 'public/js').vuetify('vuetify-loader')
+   .sass('src/sass/app.scss', 'public/css')
+   .setPublicPath('public')
+   .browserSync({
+      proxy: 'e-commerce.test',
+      files: ['public/**/index.html', 'public/css/**/app.css', 'public/js/**/app.js']
+   });
