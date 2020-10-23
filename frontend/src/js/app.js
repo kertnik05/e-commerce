0696a443b3ae4ca552b7d1fb6f7f4231a2664227
Nikko Lumahang <kertnik@gmail.com>
2020-10-23 15:11:02 +0800

    Setup Vuex

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import vuetify from './plugins/vuetify';
+import store from './plugins/store';
 import App from './components/App';
 
 new Vue({
     el: '#root',
     vuetify,
+    store,
     components: {
         App
     }
