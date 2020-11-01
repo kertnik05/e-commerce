0ee20b9d7c833f794e83451a3fb4a71a762a3c9b
JC <zxcjhaycee@gmail.com>
2020-11-01 17:22:29 +0800

    Remove unecessary code

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -2,12 +2,11 @@ window.axios = require('axios');
 axios.defaults.baseURL = 'http://api.e-commerce.test/api';
 
 import Vue from 'vue';
-import '@mdi/font/css/materialdesignicons.css'
 import vuetify from './plugins/vuetify';
 import router from './plugins/router';
 import App from '../js/App.vue';
 import store from './plugins/store';
-import App from './components/App';
+// import App from './components/App';
 
 new Vue({
     el : "#root",
