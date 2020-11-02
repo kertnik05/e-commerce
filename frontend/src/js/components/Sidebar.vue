aff038d880648468ab92fc7e551b69f4b2bef6fd
Nikko Lumahang <kertnik@gmail.com>
2020-11-02 18:55:21 +0800

    added router-link to login page in Sidebar.vue

--- frontend/src/js/components/Sidebar.vue
+++ frontend/src/js/components/Sidebar.vue
@@ -15,6 +15,12 @@
             active-class="deep-purple--text text--accent-4"
             >
             <v-list-item>
+                <v-list-item-title>
+                    <router-link to="/login">Login</router-link>
+                </v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
                 <v-list-item-title>Foo</v-list-item-title>
             </v-list-item>
 
